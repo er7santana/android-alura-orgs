2d6da5ff54822269eb4c6dc92f0e5c849225ee00
Eliezer Sant Ana <er.7santana@gmail.com>
2024-10-27 03:31:58 -0300

    refatorando nome de componentes de layout

--- app/src/main/java/br/com/shaft/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/shaft/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,6 @@
 package br.com.shaft.orgs.ui.activity
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -21,7 +20,7 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
     }
 
     private fun configuraBotaoSalvar() {
-        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        val botaoSalvar = findViewById<Button>(R.id.formulario_produto_botao_salvar)
 
         botaoSalvar.setOnClickListener {
             val produtoNovo = criaProduto()
@@ -31,13 +30,13 @@ class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario
     }
 
     private fun criaProduto(): Produto {
-        val campoNome = findViewById<EditText>(R.id.nome)
+        val campoNome = findViewById<EditText>(R.id.formulario_produto_nome)
         val nome = campoNome.text.toString()
 
-        val campoDescricao = findViewById<EditText>(R.id.descricao)
+        val campoDescricao = findViewById<EditText>(R.id.formulario_produto_descricao)
         val descricao = campoDescricao.text.toString()
 
-        var campoValor = findViewById<EditText>(R.id.valor)
+        var campoValor = findViewById<EditText>(R.id.formulario_produto_valor)
         val valorEmTexto = campoValor.text.toString()
         val valor = if (valorEmTexto.isBlank()) {
             BigDecimal.ZERO
